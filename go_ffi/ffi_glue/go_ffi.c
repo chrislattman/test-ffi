0d920d78fad5d124f0e9892886a5aebbb10b5567
Chris Lattman <natgeo13@gmail.com>
2023-10-31 02:25:55 -0400

    push

--- go_ffi/ffi_glue/go_ffi.c
+++ go_ffi/ffi_glue/go_ffi.c
@@ -1,5 +1,10 @@
 #include <dlfcn.h>
+#include <stdio.h>
 #include "go_ffi.h"
+
+void print(const char *str) {
+    printf("%s\n", str);
+}
 
 int frac_mult(Fraction *f1, Fraction *f2) {
     void *handle;
@@ -9,6 +14,8 @@ int frac_mult(Fraction *f1, Fraction *f2) {
     handle = dlopen("../../libfraction.so", RTLD_NOW);
     *(void **)(&fraction_multiply) = dlsym(handle, "fraction_multiply");
 
+    f1->print_func = print;
+    f2->print_func = print;
     retval = fraction_multiply(f1, f2);
     dlclose(handle);
     return retval;
--- go_ffi/fraction_tester.go
+++ go_ffi/fraction_tester.go
@@ -22,12 +22,12 @@ func main() {
 	frac1.numerator = 10
 	frac1.denominator = 13
 	frac1.str = C.CString("Hello")
-	frac1.print_func = C.closure(C.print)
+	frac1.print_func = C.closure(C.print) // currently doesn't do anything
 	frac2 := C.Fraction{}
 	frac2.numerator = 9
 	frac2.denominator = 17
 	frac2.str = C.CString("World!")
-	frac2.print_func = C.closure(C.print)
+	frac2.print_func = C.closure(C.print) // currently doesn't do anything
 
 	retval := C.frac_mult(&frac1, &frac2)
 	fmt.Printf("10/13 * 9/17 = %d/%d\n", frac1.numerator, frac1.denominator)
