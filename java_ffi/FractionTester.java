4fdefac944c0eeeefeb4dad6ed4e8df1d4f8dbdc
Chris Lattman <natgeo13@gmail.com>
2024-04-04 06:00:54 -0400

    update

--- java_ffi/FractionTester.java
+++ java_ffi/FractionTester.java
@@ -7,6 +7,18 @@ public class FractionTester {
         System.loadLibrary("java_ffi");
     }
 
+    static class Fraction {
+        int numerator, denominator;
+        String str;
+
+        void printFunc(String arg_string) {
+            System.out.println(arg_string);
+        }
+    }
+
+    // "Abstract" function which maps to Java_java_1ffi_FractionTester_fractionMultiply
+    private native int fractionMultiply(Fraction frac1, Fraction frac2);
+
     public static void main(String[] args) {
         Fraction frac1 = new Fraction();
         Fraction frac2 = new Fraction();
@@ -20,17 +32,5 @@ public class FractionTester {
         int retval = fractionTester.fractionMultiply(frac1, frac2);
         System.out.printf("10/13 * 9/17 = %d/%d\n", frac1.numerator, frac1.denominator);
         System.out.println("Error code = " + retval);
-    }
-
-    // "Abstract" function which maps to Java_java_1ffi_FractionTester_fractionMultiply
-    private native int fractionMultiply(Fraction frac1, Fraction frac2);
-
-    static class Fraction {
-        int numerator, denominator;
-        String str;
-
-        void printFunc(String arg_string) {
-            System.out.println(arg_string);
-        }
     }
 }
--- rust_ffi/fraction_tester.rs
+++ rust_ffi/fraction_tester.rs
@@ -1,3 +1,27 @@
+struct Fraction {
+    numerator: i32,
+    denominator: i32,
+    string: String,
+    print_func: fn(String),
+}
+
+fn rust_print(arg_string: String) {
+    println!("{}", arg_string);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut frac1 = Fraction {
+        numerator: 10,
+        denominator: 13,
+        string: "Hello".to_string(),
+        print_func: rust_print,
+    };
+    let mut frac2 = Fraction {
+        numerator: 9,
+        denominator: 17,
+        string: "World!".to_string(),
+        print_func: rust_print,
+    };
+    println!("10/13 * 9/17 = {}/{}", frac1.numerator, frac1.denominator);
+    println!("Error code = {}", 0);
 }
